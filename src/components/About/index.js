c603fb91481336ec82e14ce4540de090e398812c
Kiefer Levine <kiefer_8@hotmail.com>
2021-09-09 13:39:51 -0400

    Updated About Me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,7 +13,7 @@ const About = () => {
                     <Image src={avatar} alt='A drawn cartoon avatar.' />
                 </Col>
                 <Col md={6}>
-                    <p>I'm a budding web developer with training in the MERN stack, and ready to take on whatever technologies are thrown at me!</p>
+                    <p>A back-end web developer with a problem-solving mind, interested in making the unlikely work and the likely work better. Recently earned a certificate in full stack web development from the University of Toronto, having developed skills using HTML, CSS, JavaScript, Node.js, Express.js, MongoDB, React.js and responsive web design. I have been told by collaborators that I get things done. I worked in a team of five to develop a travel-oriented social app making use of a MySQL back-end, to create a hub for people to tell their travel stories. As an eager learner and natural “helper”, I am excited to use my technical skills and collaborative nature to write cleaner code, identify errors, and help your team continue to develop innovative, exciting work.</p>
                 </Col>
             </Row>
         </Container>
